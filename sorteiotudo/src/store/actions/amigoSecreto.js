01799e9532973f29139ca54374f0934d83b64644
Micael Andrade <micael.santos@dcomp.ufs.br>
2021-10-02 19:02:27 -0300

    Usando local storage para recuparar os amigos

--- sorteiotudo/src/store/actions/amigoSecreto.js
+++ sorteiotudo/src/store/actions/amigoSecreto.js
@@ -1,42 +1,23 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { ADD_FRIEND, DELETE_FRIEND, SORTEAR, UPDATE_FRIEND, DELETE_ALL, TOGGLE_SCREEN_SORTED, SET_FRINDS } from './actionsTypes';
+import {
+    ADD_FRIEND,
+    DELETE_FRIEND,
+    SORTEAR,
+    UPDATE_FRIEND,
+    DELETE_ALL,
+    TOGGLE_SCREEN_SORTED,
+    SET_FRINDS
+} from './actionsTypes';
 /**
  * Ações relacionadas a tela amigo secreto.
  */
-const addFriend = friend => {
-    return {
-        type: ADD_FRIEND,
-        payload: friend
-    }
-}
 
-const deleteFriend = friendId => {
-    return {
-        type: DELETE_FRIEND,
-        payload: friendId
-    }
-}
 const setFrinds = friends => {
     return {
         type: SET_FRINDS,
         payload: friends
     }
 }
-
-function getfrindStorage() {
-    return async function (dispatch){
-        const amigos = await AsyncStorage.getItem("@amigos_cadastrados")
-        console.log(amigos)
-        try {
-            return dispatch(setFrinds(JSON.parse(amigos)))
-
-        } catch (error) {
-            console.log(error)
-        }
-    }
-}
-
-
 const deleteAllFriends = _ => {
     return {
         type: DELETE_ALL
@@ -63,4 +44,66 @@ const toggle_sortear = _ => {
     }
 }
 
-export { addFriend, deleteFriend, updateFriend, sortear, deleteAllFriends, toggle_sortear, setFrinds, getfrindStorage }
+/**
+ * Recupera os amigos cadastrados no LocalStorage.
+ */
+function getfrindStorage() {
+    return async function (dispatch) {
+        const amigos = await AsyncStorage.getItem("@amigos_cadastrados")
+        try {
+            return dispatch(setFrinds(JSON.parse(amigos)))
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+/**
+ * Adiciona um amigo ao Local Storage. 
+ * TODO: Tratamento de erros.
+ */
+const addStorageFriend = (name, email) => {
+
+    return async function (dispach) {
+        const amigos = await AsyncStorage.getItem("@amigos_cadastrados") || '[]'
+        const amigosArray = JSON.parse(amigos)
+        amigosArray.push({ name, email, id: Math.random() })
+        await AsyncStorage.setItem("@amigos_cadastrados", JSON.stringify(amigosArray))
+        try {
+            return dispach(getfrindStorage())
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+/**
+ * Deleta um amigo do LocalStorage.
+ */
+const deleteStorageFriend = (key) => {
+    return async function (dispach) {
+        const amigos = await AsyncStorage.getItem("@amigos_cadastrados") || '[]'
+        let amigosArray = JSON.parse(amigos)
+        amigosArray = amigosArray.filter(({ id }) => (id != key))
+        await AsyncStorage.setItem("@amigos_cadastrados", JSON.stringify(amigosArray))
+
+        try {
+            return dispach(getfrindStorage())
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+
+
+export {
+    updateFriend,
+    sortear,
+    deleteAllFriends,
+    toggle_sortear,
+    setFrinds,
+    getfrindStorage,
+    addStorageFriend,
+    deleteStorageFriend
+}
