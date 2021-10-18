30fe9b4b8241652fbf297702690946a9e376974b
Micael Andrade <micael.santos@dcomp.ufs.br>
2021-10-18 20:55:20 -0300

    ajustes layout code

--- sorteiotudo/index.js
+++ sorteiotudo/index.js
@@ -3,18 +3,17 @@
  */
 import 'react-native-gesture-handler';
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import { Provider } from 'react-redux';
+import {AppRegistry} from 'react-native';
+import {Provider} from 'react-redux';
 import Navegador from './src/Navigator';
 import storeConfig from './src/store/storeConfig';
 
-
-import { name as appName } from './app.json';
+import {name as appName} from './app.json';
 
 const store = storeConfig();
 const Redux = _ => (
-    <Provider store={store}>
-        <Navegador />
-    </Provider>
-)
+  <Provider store={store}>
+    <Navegador />
+  </Provider>
+);
 AppRegistry.registerComponent(appName, () => Redux);
