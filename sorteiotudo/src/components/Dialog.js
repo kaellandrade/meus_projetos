20ce980dbc010c0da1e1b261a2187276ee1a4968
Micael Andrade <micael.santos@dcomp.ufs.br>
2021-10-18 20:55:54 -0300

    modo dark

--- sorteiotudo/src/components/Dialog.js
+++ sorteiotudo/src/components/Dialog.js
@@ -1,39 +1,62 @@
 import React from 'react';
-
-import {
-    AlertDialog,
-    Button
-} from 'native-base';
-
+import {StyleSheet, Text} from 'react-native';
+import {AlertDialog, Button} from 'native-base';
 
 const Dialog = props => {
-   
-    return (
-        <AlertDialog
-            leastDestructiveRef={props.cancelRef}
-            isOpen={props.AlertisOpen}
-            onClose={props.onClose}
-            motionPreset={"fade"}
+  const isDark = props.dark;
+  console.log(isDark);
+  return (
+    <AlertDialog
+      leastDestructiveRef={props.cancelRef}
+      isOpen={props.AlertisOpen}
+      onClose={props.onClose}
+      motionPreset={'fade'}>
+      <AlertDialog.Content
+        style={isDark ? estilos.darkModeContent : estilos.whiteModeContent}>
+        <AlertDialog.Header fontSize="lg" fontWeight="bold">
+          <Text style={isDark ? estilos.darkModeFont : estilos.whiteModeFont}>
+            Deletar amigos
+          </Text>
+        </AlertDialog.Header>
+        <AlertDialog.Body>
+          <Text style={isDark ? estilos.darkModeFont : estilos.whiteModeFont}>
+            Você realmente deseja deletar todos seus amigos cadastrados?
+          </Text>
+        </AlertDialog.Body>
+        <AlertDialog.Footer
+          style={isDark ? estilos.darkModeFooter : estilos.whiteModeContent}>
+          <Button ref={props.cancelRef} onPress={props.onClose}>
+            Não
+          </Button>
+          <Button colorScheme="red" onPress={props.deleteAllFriends} ml={3}>
+            Sim
+          </Button>
+        </AlertDialog.Footer>
+      </AlertDialog.Content>
+    </AlertDialog>
+  );
+};
 
-        >
-            <AlertDialog.Content>
-                <AlertDialog.Header fontSize="lg" fontWeight="bold">
-                    Deletar amigos
-                </AlertDialog.Header>
-                <AlertDialog.Body>
-                    Você realmente deseja deletar todos seus amigos cadastrados?
-                </AlertDialog.Body>
-                <AlertDialog.Footer>
-                    <Button ref={props.cancelRef} onPress={props.onClose}>
-                        Não
-                    </Button>
-                    <Button colorScheme="red" onPress={props.deleteAllFriends} ml={3}>
-                        Sim
-                    </Button>
-                </AlertDialog.Footer>
-            </AlertDialog.Content>
-        </AlertDialog >
-    );
-}
+const estilos = StyleSheet.create({
+  darkModeContent: {
+    backgroundColor: '#222831',
+  },
+  darkModeFooter: {
+    backgroundColor: '#132743',
+  },
+  darkModeFont: {
+    color: '#fffa',
+  },
+  whiteModeFont: {
+    color: '#000',
+  },
+  whiteModeContent: {
+    backgroundColor: '#FFF',
+  },
+  title: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
 
 export default Dialog;
