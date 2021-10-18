572aeca030d8bf44d12448e23b7194d0bd0b7586
Micael Andrade <micael.santos@dcomp.ufs.br>
2021-10-18 20:56:14 -0300

    ajustes layout

--- sorteiotudo/src/components/ModalFrinds.js
+++ sorteiotudo/src/components/ModalFrinds.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Button, Modal, Input, useToast } from "native-base";
-import { connect } from "react-redux";
-import { closeModal } from "../store/actions/modal";
+import React, {useEffect, useRef, useState} from 'react';
+import {Button, Modal, Input, useToast} from 'native-base';
+import {connect} from 'react-redux';
+import {closeModal} from '../store/actions/modal';
 import {
-    updateStorageFriend,
-    addStorageFriend
+  updateStorageFriend,
+  addStorageFriend,
 } from '../store/actions/amigoSecreto';
-import { VAZIO } from '../util/constantes';
-import { Dimensions, Text, StyleSheet } from "react-native";
-import { validateEmail } from '../validations/validateFuncs';
+import {VAZIO} from '../util/constantes';
+import {Dimensions, Text, StyleSheet} from 'react-native';
+import {validateEmail} from '../validations/validateFuncs';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import If from '../components/If';
 /**
@@ -16,165 +16,166 @@ import If from '../components/If';
  */
 
 const ModalFrind = props => {
-    const inputName = useRef(null)
-    const inputEmail = useRef(null)
-    const toast = useToast();
-    const darkMode = props.darkMode;
-    let [name, setName] = useState('')
-    let [email, setEmail] = useState('')
+  const inputName = useRef(null);
+  const inputEmail = useRef(null);
+  const toast = useToast();
+  const darkMode = props.darkMode;
+  let [name, setName] = useState('');
+  let [email, setEmail] = useState('');
 
-    useEffect(_ => {
-        if (props.updateMode) {
-            setName(String(props.name))
-            setEmail(String(props.email))
-        } else {
-            setName('')
-            setEmail('')
-        }
+  useEffect(
+    _ => {
+      if (props.updateMode) {
+        setName(String(props.name));
+        setEmail(String(props.email));
+      } else {
+        setName('');
+        setEmail('');
+      }
+    },
+    [props.updateMode, props.name, props.email],
+  );
 
-    }, [props.updateMode])
-
-
-
-    const updateOrAdd = async _ => {
-        if (props.id !== undefined) {
-            props.updateFriend({ name, email, id: props.id })
-            props.closeModal()
-        } else { // Modo ADD
-            if (validateEmail(email)) {
-                props.addFriend({ name, email })
-                setName('')
-                setEmail('')
-                toast.show({
-                    title: `${name}, adicionado(a)!`,
-                    placement: 'top',
-                    status: "success",
-                    width: Dimensions.get('window').width
-                })
-                inputName.current.focus();
-            } else {
-                toast.show({
-                    title: 'Ops! E-mail inválido!',
-                    placement: 'top',
-                    status: "error",
-                    width: Dimensions.get('window').width,
-                    accessibilityLiveRegion: 'assertive'
-                })
-                inputEmail.current.focus()
-
-            }
-        }
+  const updateOrAdd = async _ => {
+    if (props.id !== undefined) {
+      props.updateFriend({name, email, id: props.id});
+      props.closeModal();
+    } else {
+      // Modo ADD
+      if (validateEmail(email)) {
+        props.addFriend({name, email});
+        setName('');
+        setEmail('');
+        toast.show({
+          title: `${name.trim()}, adicionado(a)!`,
+          placement: 'top',
+          status: 'success',
+          width: Dimensions.get('window').width,
+        });
+        inputName.current.focus();
+      } else {
+        toast.show({
+          title: 'Ops! E-mail inválido!',
+          placement: 'top',
+          status: 'error',
+          width: Dimensions.get('window').width,
+          accessibilityLiveRegion: 'assertive',
+        });
+        inputEmail.current.focus();
+      }
     }
-    return (
-        <Modal
-            isOpen={props.showModal}
-            onClose={props.closeModal}
-            size='lg'
-
-        >
-            <Modal.Content style={darkMode ? estilos.darkModeContent : estilos.whiteModeContent}>
-                <Modal.CloseButton />
-                <Modal.Header>
-                    <Text style={[estilos.title, darkMode ? estilos.darkModeFont : estilos.whiteModeFont]}>
-                        {props.name ? 'Atualize seu amigo!' : 'Insira um novo amigo!'}
-                    </Text>
-                </Modal.Header>
-                <Modal.Body>
-                    <If condition={!props.name}>
-                        <Text style={darkMode ? estilos.darkModeFont : estilos.whiteModeFont}>
-                            Ah, lembre-se de colocar um email válido! 😉
-                        </Text>
-                    </If>
-                    <Input
-                        style={darkMode ? { color: 'white' } : estilos.whiteModeFont}
-                        mt={4}
-                        placeholder="Nome do amigo..."
-                        value={name}
-                        onChangeText={name => setName(name)}
-                        autoFocus
-                        ref={inputName}
-                        InputLeftElement={
-
-                            <Icon name='user' size={20} style={{ margin: 5 }} color='gray' />
-                        }
-
-                    />
-                    <Input
-                        style={darkMode ? { color: 'white' } : estilos.whiteModeFont}
-                        ref={inputEmail}
-                        mt={4}
-                        placeholder="E-mail do amigo"
-                        value={email}
-                        onChangeText={email => setEmail(email)}
-                        keyboardType="email-address"
-                        autoCapitalize='none'
-                        InputLeftElement={
-                            <Icon name='envelope' size={20} style={{ margin: 5 }} color='gray' />
-                        }
-                    />
-                </Modal.Body>
-                <Modal.Footer
-                    style={darkMode ? estilos.darkModeFooter : estilos.whiteModeContent}
-                >
-                    <Button.Group>
-                        <Button
-                            onPress={_ => props.closeModal()}
-                            colorScheme="secondary"
-                        >
-                            Fechar
-                        </Button>
-                        <Button
-                            onPress={updateOrAdd}
-                            isDisabled={name.trim().length === VAZIO || email.trim().length === VAZIO}
-                        >
-                            {props.name ? 'Atualizar' : 'Cadastrar'}
-                        </Button>
-                    </Button.Group>
-                </Modal.Footer>
-            </Modal.Content>
-        </Modal>
-    )
-}
+  };
+  return (
+    <Modal isOpen={props.showModal} onClose={props.closeModal} size="lg">
+      <Modal.Content
+        style={darkMode ? estilos.darkModeContent : estilos.whiteModeContent}>
+        <Modal.CloseButton />
+        <Modal.Header>
+          <Text
+            style={[
+              estilos.title,
+              darkMode ? estilos.darkModeFont : estilos.whiteModeFont,
+            ]}>
+            {props.name ? 'Atualize seu amigo!' : 'Insira um novo amigo!'}
+          </Text>
+        </Modal.Header>
+        <Modal.Body>
+          <If condition={!props.name}>
+            <Text
+              style={darkMode ? estilos.darkModeFont : estilos.whiteModeFont}>
+              Ah, lembre-se de inserir um email válido! 😉
+            </Text>
+          </If>
+          <Input
+            style={darkMode ? {color: 'white'} : estilos.whiteModeFont}
+            mt={4}
+            placeholder="Nome do amigo..."
+            value={name}
+            onChangeText={name => setName(name)}
+            autoFocus
+            ref={inputName}
+            InputLeftElement={
+              <Icon name="user" size={20} style={{margin: 5}} color="gray" />
+            }
+          />
+          <Input
+            style={darkMode ? {color: 'white'} : estilos.whiteModeFont}
+            ref={inputEmail}
+            mt={4}
+            placeholder="E-mail do amigo"
+            value={email}
+            onChangeText={email => setEmail(email)}
+            keyboardType="email-address"
+            autoCapitalize="none"
+            InputLeftElement={
+              <Icon
+                name="envelope"
+                size={20}
+                style={{margin: 5}}
+                color="gray"
+              />
+            }
+          />
+        </Modal.Body>
+        <Modal.Footer
+          style={darkMode ? estilos.darkModeFooter : estilos.whiteModeContent}>
+          <Button.Group>
+            <Button onPress={_ => props.closeModal()} colorScheme="secondary">
+              Fechar
+            </Button>
+            <Button
+              onPress={updateOrAdd}
+              isDisabled={
+                name.trim().length === VAZIO || email.trim().length === VAZIO
+              }>
+              {props.name ? 'Atualizar' : 'Cadastrar'}
+            </Button>
+          </Button.Group>
+        </Modal.Footer>
+      </Modal.Content>
+    </Modal>
+  );
+};
 
 const estilos = StyleSheet.create({
-    darkModeContent: {
-        backgroundColor: "#222831"
-    },
-    darkModeFooter: {
-        backgroundColor: '#132743'
-    },
-    darkModeFont: {
-        color: '#fffa'
-    },
-    whiteModeFont: {
-        color: '#000'
-    },
-    whiteModeContent: {
-        backgroundColor: '#FFF'
-    },
-    title: {
-        fontWeight: 'bold',
-        textAlign: 'center'
-    }
-})
+  darkModeContent: {
+    backgroundColor: '#222831',
+  },
+  darkModeFooter: {
+    backgroundColor: '#132743',
+  },
+  darkModeFont: {
+    color: '#fffa',
+  },
+  whiteModeFont: {
+    color: '#000',
+  },
+  whiteModeContent: {
+    backgroundColor: '#FFF',
+  },
+  title: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+});
 
-const mapStateToProps = ({ modal, config }) => {
-    return {
-        showModal: modal.showModal,
-        updateMode: modal.updateMode,
-        name: modal.updateMode.name,
-        email: modal.updateMode.email,
-        id: modal.updateMode.id,
-        darkMode: config.darkMode
-    }
-}
+const mapStateToProps = ({modal, config}) => {
+  return {
+    showModal: modal.showModal,
+    updateMode: modal.updateMode,
+    name: modal.updateMode.name,
+    email: modal.updateMode.email,
+    id: modal.updateMode.id,
+    darkMode: config.darkMode,
+  };
+};
 const mapDispatchToProps = dispach => {
-    return {
-        closeModal: _ => dispach(closeModal()),
-        addFriend: ({ name, email }) => dispach(addStorageFriend({ name, email })),
-        updateFriend: ({ name, email, id }) => dispach(updateStorageFriend({ name, email, id }))
-
-    }
-}
+  return {
+    closeModal: _ => dispach(closeModal()),
+    addFriend: ({name, email}) => dispach(addStorageFriend({name, email})),
+    updateFriend: ({name, email, id}) =>
+      dispach(updateStorageFriend({name, email, id})),
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(ModalFrind);
